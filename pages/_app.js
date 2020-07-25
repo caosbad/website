2691d2d9e61eec37159e9396022383ae51ff8bea
苏涛 <first@taosue.xyz>
2020-07-26 03:50:24 +0800

    upate

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/styles.sass'
-import './i18n';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import enUsTrans from "../public/locales/en-us.json";
+import zhCnTrans from "../public/locales/zh-cn.json";
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
+    resources: {
+        en: {
+            translation: enUsTrans,
+        },
+        zh: {
+            translation: zhCnTrans,
+        },
+    },
+    fallbackLng: "en",
+  debug: false,
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  },
+})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
